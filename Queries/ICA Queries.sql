a2f586f8b64e0ae8935bfb511bc1605b32d303ce
Carl Baines <carltbaines@hotmail.co.uk>
2025-04-04 17:10:26 +0100

    Second Commit

--- Queries/ICA Queries.sql
+++ Queries/ICA Queries.sql
@@ -20,19 +20,19 @@ WHERE TABLE_NAME = 'match';
 
 --Demo A1 Query One
 --Module 3: Writing SELECT queries with single table.
---User Story: Select all data from every column and row from the EuroLeagues.league table.
+--Description: Select all data from every column and row from the EuroLeagues.league table.
 SELECT * FROM league;
 
 --Demo A1 Query Two
 --Module 3: Writing SELECT queries with single table.
---User Story: Select the total number of goals scored from the EuroLeagues.match table.
+--Description: Select the total number of goals scored from the EuroLeagues.match table.
 --Explanation: Simple SELECT query that creates a calculated column, calling the sum function on the home_team_goal and away_team_goal columns.
 SELECT SUM(home_team_goal + away_team_goal)
 FROM match;
 
 --Demo A2 Query One
 --Module 3: Eliminating Duplicates with DISTINCT
---User Story: Eliminate duplicate seasons from the EuroLeagues.match table and order them from earliest to latest.
+--Description: Eliminate duplicate seasons from the EuroLeagues.match table and order them from earliest to latest.
 --Explanation: This query uses a subquery and casts the season as a varchar type (it was initially stored as a text value), so that it can work directly with functions like LEFT().
 --It extracts the starting years of the seasons (the first four characters), casts them to an int and orders them.
 SELECT season
@@ -44,26 +44,26 @@ ORDER BY CAST(LEFT(season, 4) AS INT);
 
 --Demo A2 Query Two
 --Module 3: Eliminating Duplicates with DISTINCT
---User Story: Select unique player names from the EuroLeagues.player table and stores them in a column called 'AllPlayerNames'.
+--Description: Select unique player names from the EuroLeagues.player table and stores them in a column called 'AllPlayerNames'.
 SELECT DISTINCT CAST(player_name AS VARCHAR(MAX)) AS AllPlayerNames
 FROM player;
 
 --Demo A3 Query One
 --Module 3: Using Column and Table Aliases Lesson
---User Story: Select the total number of goals scored from the EuroLeagues.match table and assign the column the 'TotalGoalsScored' alias.
+--Description: Select the total number of goals scored from the EuroLeagues.match table and assign the column the 'TotalGoalsScored' alias.
 SELECT SUM(home_team_goal + away_team_goal) AS TotalGoalsScored
 FROM match;
 
 --Demo A3 Query Two
 --Module 3: Using Column and Table Aliases Lesson
---User Story: Select all columns from the EuroLeagues.team table using the alias 'MiddlesbroughFCInfo', where the team_long_name is Middlesbrough.
+--Description: Select all columns from the EuroLeagues.team table using the alias 'MiddlesbroughFCInfo', where the team_long_name is Middlesbrough.
 SELECT id, team_api_id, team_fifa_api_id, team_long_name, team_short_name
 FROM team AS MiddlesbroughFCInfo
 WHERE CAST(team_long_name AS VARCHAR(MAX)) = 'Middlesbrough';
 
 --Demo A4 Query One
 --Module 4: Writing Simple CASE expressions
---User Story: Categorise countries by league tier.
+--Description: Categorise countries by league tier.
 --The name of the countries is casted as a varchar so that it can work directly with functions.
 SELECT CAST(name AS VARCHAR(MAX)) AS country_names,
 	CASE
@@ -74,7 +74,7 @@ FROM country;
 
 --Demo A4 Query Two
 --Module 4: Writing Simple CASE expressions
---User Story: Determine the result of a match using the match table.
+--Description: Determine the result of a match using the match table.
 SELECT id AS match_id,
 	CASE
 		WHEN home_team_goal > away_team_goal THEN 'Home Team Won'
@@ -85,7 +85,7 @@ FROM match;
 
 --Demo B1 Query One
 --Module 1: How to provide data from 2 related tables with a Join.
---User Story: Select the league names associated with each country.
+--Description: Select the league names associated with each country.
 SELECT c.name AS country_name, l.name AS league_name
 FROM country AS c
 JOIN league AS l
@@ -103,7 +103,7 @@ ORDER BY p.player_api_id;
 
 --Demo B2 Query One
 --Module 4: How to query with inner joins.
---User Story: Select all the different ratings of the best player (the best player has the highest overall and potential ratings)
+--Description: Select all the different ratings of the best player (the best player has the highest overall and potential ratings)
 --Lionel Messi.
 SELECT p.player_api_id, CAST(p.player_name AS varchar(MAX)) AS player_name, pa.overall_rating, pa.potential AS potential_rating
 FROM player AS p
@@ -114,7 +114,7 @@ ORDER BY p.player_api_id
 
 --Demo B2 Query Two
 --Module 4: How to query with inner joins
---User Story: Join the match table with the team table to get the home and away team names.
+--Description: Join the match table with the team table to get the home and away team names.
 SELECT DISTINCT team_api_id, CAST(team_long_name AS varchar(MAX)) AS team_long_name
 FROM team
 JOIN match
@@ -123,33 +123,119 @@ ORDER BY team_api_id;
 
 --Demo B3 Query One
 --Module 4: How to query with outer joins
---User Story: left join between team and team_attributes to retrieve a distinct list of all teams, including those with or without associated attribute data.
+--Description: full outer join between team and team_attributes to retrieve a distinct list of all teams, including those with or without associated attribute data.
 SELECT DISTINCT t.team_fifa_api_id, CAST(t.team_long_name AS varchar(MAX)) AS team_long_name, CAST(t.team_short_name AS varchar(MAX)) AS team_short_name
 FROM team AS t
-LEFT JOIN team_attributes AS ta
+FULL OUTER JOIN team_attributes AS ta
 ON t.team_fifa_api_id = ta.team_fifa_api_id;
 
 --Demo B3 Query Two
 --Module 4: How to query with outer joins
---User Story: left join between match and team to retrieve a distinct list of all matches, ensuring that match data is included even if team details are duplicated or missing due to the join condition.
+--Description: full outer join between match and team to retrieve a distinct list of all matches, ensuring that match data is included even if team details are duplicated or missing due to the join condition.
 SELECT DISTINCT CAST(m.mdate AS varchar(MAX)) AS match_date, m.match_api_id, m.home_team_api_id, m.away_team_api_id, m.home_team_goal, m.away_team_goal
 FROM match AS m
-LEFT JOIN team AS t
+FULL OUTER JOIN team AS t
 ON m.home_team_api_id = t.team_api_id OR m.away_team_api_id = t.team_api_id;
 
 --Demo B4 Query One
 --Module 4: How to query with cross and self joins
+--Description: Retrieves distinct player names, their fifa API ids, overall ratings, and preferred foot by cross joining the player and player_attributes tables together.
+SELECT DISTINCT CAST(p.player_name AS varchar(MAX)) AS player_name, p.player_fifa_api_id, pa.overall_rating, CAST(pa.preferred_foot AS varchar(MAX)) AS preferred_foot
+FROM player AS p
+CROSS JOIN player_attributes AS pa
+WHERE p.player_api_id = pa.player_api_id;
 
+--Demo B4 Query Two
+--Module 4: How to query with cross and self joins 
+--Description: Retrieves top 100 distinct player comparisons between a specific player and other players based on their finishing and shot power attributes.
+--The player for comparison is Patryk Rachwal.
+SELECT DISTINCT TOP 100 
+	   p1.player_api_id AS p1_api_id,
+	   CAST(p1_player.player_name AS varchar(MAX)) AS player_for_comparison,
+	   p1.finishing,
+	   p1.shot_power,
+	   p2.player_api_id AS p2_api_id,
+	   CAST(p2_player.player_name AS varchar(MAX)) AS p2_name,
+	   p2.finishing,
+	   p2.shot_power
+FROM player_attributes p1
+JOIN player_attributes p2
+	ON p1.player_api_id = 2625 AND p1.player_api_id <> p2.player_api_id
+JOIN player p1_player ON p1.player_api_id = p1_player.player_api_id
+JOIN player p2_player ON p2.player_api_id = p2_player.player_api_id
+ORDER BY p2.player_api_id;
 
-
-
-
---Unrelated Queries - I like them so will add to portfolio at some point.
---User Story: Select id and player names from the EuroLeagues.player table, ordering the id in ascending order.
+--Demo C1 Query One
+--Module 5: How to Sort Data
+--Description: Select id and player names from the EuroLeagues.player table, ordering the id in ascending order.
 --Simple SELECT query with ORDER by clause.
 SELECT id, player_name
 FROM player
 ORDER BY id ASC;
 
+--Demo C1 Query Two
+--Module 5: How to Sort Data
+--Description: Selects the player_api_id, name and height from the EuroLeagues.player table, ordering the players by height in descending order.
+SELECT player_api_id, player_name, height
+FROM player
+ORDER BY height DESC;
 
+--Demo C2 Query One
+--Module 5: How to filter data with predicates.
+--Description: Retrieves a list of distinct players that have an overall rating greater than 80. Each player only appears once with their highest rating.
+SELECT DISTINCT pa.player_fifa_api_id, CAST(p.player_name AS varchar(MAX)) AS player_name, MAX(pa.overall_rating) AS overall_rating
+FROM player_attributes AS pa
+JOIN player AS p
+ON p.player_fifa_api_id = pa.player_fifa_api_id
+WHERE overall_rating > 80
+GROUP BY pa.player_fifa_api_id, CAST(p.player_name AS varchar(MAX));
 
+--Demo C2 Query Two
+--Module 5: How to filter data with predicates.
+--Description: The query retrieves all match details involving Middlesbrough, including the teams they played, the season (as well as its stage), the matchID and the goals scored.
+SELECT
+	home_team.team_api_id AS home_team_api_id,
+	CAST(home_team.team_long_name AS VARCHAR(MAX)) AS home_team,
+	away_team.team_api_id AS away_team_api_id,
+	CAST(away_team.team_long_name AS VARCHAR(MAX)) AS away_team,
+	CAST(m.season AS VARCHAR(MAX)) AS season,
+	m.stage,
+	m.match_api_id,
+	m.home_team_goal,
+	m.away_team_goal
+FROM match AS m
+JOIN team AS home_team ON home_team.team_api_id = m.home_team_api_id
+JOIN team AS away_team ON away_team.team_api_id = m.away_team_api_id
+WHERE 
+	CAST(home_team.team_long_name AS varchar(MAX)) = 'Middlesbrough'
+	OR CAST(away_team.team_long_name AS varchar(MAX)) = 'Middlesbrough';
+
+--Demo C3 Query One
+--Module 5: How to filter data with TOP and OFFSET-FETCH.
+--Description: Selects top 100 players and their api ids from the players table, joining with the player attributes table to get their overall ratings.
+--Their overall ratings are ordered in descending order.
+SELECT DISTINCT TOP 100 CAST(p.player_name AS nvarchar(MAX)) AS player_name, p.player_api_id, ISNULL(pa.overall_rating, '0') AS overall_rating
+FROM player AS p
+JOIN player_attributes AS pa
+ON p.player_api_id = pa.player_api_id
+GROUP BY CAST(p.player_name AS nvarchar(MAX)), p.player_api_id, pa.overall_rating
+ORDER BY overall_rating DESC
+
+--Demo C3 Query Two
+--Module 5: How to filter data with TOP and OFFSET-FETCH
+--Description: Selects the bottom 10 of the top 100 players with their api ids from the players table, joining with the players attributes table to get their overall ratings.
+SELECT DISTINCT CAST(p.player_name AS nvarchar(MAX)) AS player_name, p.player_api_id, ISNULL(pa.overall_rating, '0') AS overall_rating
+FROM player AS p
+JOIN player_attributes AS pa
+ON p.player_api_id = pa.player_api_id
+GROUP BY CAST(p.player_name AS nvarchar(MAX)), p.player_api_id, pa.overall_rating
+ORDER BY overall_rating DESC
+OFFSET 90 ROWS
+FETCH NEXT 10 ROWS ONLY;
+
+--Demo C4 Query One
+--Module 5: How to work with unknown values
+--Description: Selects the team_fifa_api_id, the long name and short name of teams from the teams table. 
+--If the team_fifa_api_id is null, the string 'No FIFA API ID' is replaced in place of the null value.
+SELECT ISNULL(CAST(TRY_CAST(team_fifa_api_id AS INT) AS VARCHAR(255)), 'No FIFA API ID') AS fifa_api_id, team_long_name, team_short_name
+FROM team;
